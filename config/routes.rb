cfdba0fe03b4e60ecc9f7fc0925bdcc0e9ee8f35
heraldofortuna <heraldofortuna@gmail.com>
2021-01-17 20:11:47 -0500

    Routes of posts and categories were created

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 Rails.application.routes.draw do
-  get 'categories/new'
-  get 'categories/create'
-  get 'categories/edit'
-  get 'categories/update'
-  get 'categories/destroy'
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/edit'
-  get 'posts/update'
-  get 'posts/destroy'
+  resources :posts do
+    resources :categories, only: [:new, :create, :edit, :update, :destroy]
+  end
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
